5fda4e6baa279d48fdb4b1615b9d2bbff1d3a3e6
mark thueson <thueson@thueson-VirtualBox.(none)>
2013-01-26 17:40:12 -0700

    added hw1

---
+++ hw1/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/thueson/cs6460/hw1/hello.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-36-generic-pae/scripts/module-common.lds --build-id  -o /home/thueson/cs6460/hw1/hello.ko /home/thueson/cs6460/hw1/hello.o /home/thueson/cs6460/hw1/hello.mod.o
